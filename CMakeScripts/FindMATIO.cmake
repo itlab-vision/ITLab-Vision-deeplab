db4be1810f2813df69de19d7d9ecd6ea6932da2e
valentina-kustikova <valentina.kustikova@gmail.com>
2015-11-05 13:22:57 +0300

    Updated scripts (checked on cluster).

--- CMakeScripts/FindMATIO.cmake
+++ CMakeScripts/FindMATIO.cmake
@@ -6,7 +6,7 @@
 # The following are set after configuration is done:
 #  MATIO_FOUND
 #  MATIO_INCLUDE_DIR
-#  MATIO_LIBRARY_DIR
+#  MATIO_LIBRARIES
 
 include(FindPackageHandleStandardArgs)
 
@@ -15,18 +15,18 @@ set(MATIO_ROOT_DIR "" CACHE PATH "Folder contains MatIO")
 find_path(MATIO_INCLUDE_DIR matio.h
     PATHS ${MATIO_ROOT_DIR})
 
-find_library(MATIO_LIBRARY_DIR matio
-        PATHS ${MATIO_ROOT_DIR}
-        PATH_SUFFIXES
-            lib)
-
-MESSAGE(STATUS "MATIO include:" ${MATIO_INCLUDE_DIR})
-MESSAGE(STATUS "MATIO lib:" ${MATIO_LIBRARY_DIR})
+find_library(MATIO_LIBRARY matio
+    PATHS ${MATIO_ROOT_DIR}
+    PATH_SUFFIXES
+        lib)
 
 find_package_handle_standard_args(MATIO DEFAULT_MSG
-    MATIO_INCLUDE_DIR MATIO_LIBRARY_DIR)
+    MATIO_INCLUDE_DIR MATIO_LIBRARY)
+
+MESSAGE(STATUS "MATIO include:" ${MATIO_INCLUDE_DIR})
+MESSAGE(STATUS "MATIO lib:" ${MATIO_LIBRARY})
 
 if(MATIO_FOUND)
     set(MATIO_INCLUDE_DIR ${MATIO_INCLUDE_DIR})
-    set(MATIO_LIBRARY_DIR ${MATIO_LIBRARY_DIR})
+    set(MATIO_LIBRARIES ${MATIO_LIBRARY})
 endif()
