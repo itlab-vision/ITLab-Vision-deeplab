846ba4c8ac9b505514be6a5f8e9f1b8d65ce74df
valentina-kustikova <valentina.kustikova@gmail.com>
2015-11-12 09:52:49 +0300

    Minor cleanings in sbatch scripts.

--- extra/scripts/run_densecrf_sbatch.sh
+++ extra/scripts/run_densecrf_sbatch.sh
@@ -4,6 +4,8 @@ TASK=${PWD}/run_densecrf.sh
 FILE=${PWD}/log_densecrf.txt
 TIME=3000
 
-echo "Command line: sbatch -p "all" -N 1 -o ${FILE} ${TASK}"
+CMD="sbatch -p \"all\" -t ${TIME} -N 1 -o ${FILE} ${TASK}"
 
-sbatch -p "all" -t ${TIME} -N 1 -o ${FILE} ${TASK}
+echo "Command line: ${CMD}"
+
+${CMD}
--- extra/scripts/run_pascal_sbatch.sh
+++ extra/scripts/run_pascal_sbatch.sh
@@ -4,6 +4,8 @@ TASK=${PWD}/run_pascal.sh
 FILE=${PWD}/log.txt
 TIME=3000
 
-echo "Command line: sbatch -p "gpu" -N 1 -o ${FILE} ${TASK}"
+CMD="sbatch -p \"gpu\" -t ${TIME} -N 1 -o ${FILE} ${TASK}"
 
-sbatch -p "gpu" -t ${TIME} -N 1 -o ${FILE} ${TASK}
+echo "Command line: ${CMD}"
+
+${CMD}
