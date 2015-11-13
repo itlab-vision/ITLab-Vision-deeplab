5f587dcc5cf60a4afb9145872f2a12ead313a331
valentina-kustikova <valentina.kustikova@gmail.com>
2015-11-13 15:44:01 +0300

    Bug fix in create_list.sh.

--- extra/scripts/create_list.sh
+++ extra/scripts/create_list.sh
@@ -7,5 +7,5 @@ IMGS_SUB_DIR=imgs_track_10_5000-7000
 TEST_SET=test
 
 cd ${IMGS_DIR}/${IMGS_SUB_DIR}
-(ls | sed "s|^|${IMGS_SUB_DIR}/|") > ${EXP_DIR}/${DATASET}/list/${TEST_SET}.txt
+(ls | sed "s|^|/${IMGS_SUB_DIR}/|") > ${EXP_DIR}/${DATASET}/list/${TEST_SET}.txt
 (ls -1 | sed "s|\(.*\)\..*|\1|")  > ${EXP_DIR}/${DATASET}/list/${TEST_SET}_id.txt
