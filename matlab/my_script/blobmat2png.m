f7408bf2e37c6ff7e83250d9278d6b40194abc39
zhiltsov-max <zhiltsov.max35@gmail.com>
2015-11-15 22:18:24 +0300

    Update blobmat2png.m
    updated

--- matlab/my_script/blobmat2png.m
+++ matlab/my_script/blobmat2png.m
@@ -7,16 +7,13 @@
 
 function [] = blobmat2png(mat_dir, init_jpg_dir, color_map_file, png_dir)
 
-if (exist(png_dir, 'dir') == false)
-    mkdir(png_dir);
-end
-
 % list all .mat-files
 mat_files = dir(mat_dir);
 % list all .jpg-files
 jpg_files = dir(init_jpg_dir);
 % load PASCAL colormap to fill segments
 color_map = load(color_map_file);
+color_map = color_map.colormap;
 
 for i = 1 : length(mat_files)
     if (mat_files(i).isdir)
@@ -25,7 +22,6 @@ for i = 1 : length(mat_files)
 
     % load .mat file data
     mat_file_name = fullfile(mat_dir, mat_files(i).name);
-    fprintf(1, 'Processing %s...', mat_file_name);
     score_map = load(mat_file_name);
 
     % load initial image size
@@ -38,11 +34,11 @@ for i = 1 : length(mat_files)
     raw_result = permute(score_map.data, [2 1 3]);
     result = raw_result(1 : img_row, 1 : img_col, :);    
     [~, max_nchannel] = max(result, [], 3);
-    disp(max_nchannel(1: 10, 1:10));
-    
+    max_nchannel = uint8(max_nchannel) - 1;
+
     % write data to .png-file
     index = strfind(mat_files(i).name, '_blob_');
     file_name = mat_files(i).name(1 : index(1) - 1);
     png_file_name = fullfile(png_dir, [file_name, '.png']);
-    imwrite(uint8(max_nchannel), color_map.colormap, png_file_name);
+    imwrite(uint8(max_nchannel), color_map, png_file_name);
 end
