5b99bba94c34c6eb2d7181453c0221e12eca5e28
zhiltsov-max <zhiltsov.max35@gmail.com>
2016-06-04 09:59:59 +0300

    update

--- extra/scripts/segmentation_accuracy.py
+++ extra/scripts/segmentation_accuracy.py
@@ -1,172 +1,169 @@
-'''
+"""
 Usage: python segmentation_accuracy.py segmentation/dir gt/dir images_list.txt
 
-'''
+This tool calculates a IoU score for set of given images
+using a formula equivalent to intersection / union.
+The result is the same as in Pascal VOC 2012 Challenge scripts.
+"""
 
 
 from PIL import Image, ImageStat
 import sys
 import os
-
+import numpy
 
 
 def show_help():
-	print(__doc__)
+    print(__doc__)
 
 
 class SegmentationResultsProcessor:
-	Classes = [
-		'background',
-		'aeroplane',
-		'bicycle',
-		'bird',
-		'boat',
-		'bottle',
-		'bus',
-		'car',
-		'cat',
-		'chair',
-		'cow',
-		'diningtable',
-		'dog',
-		'horse',
-		'motorbike',
-		'person',
-		'pottedplant',
-		'sheep',
-		'sofa',
-		'train',
-		'tvmonitor'
-	]
+    """
+    For customizing a set of existing classes specify
+    classes field.
 
-	results_directory = None
-	gt_directory = None
-	list_path = None
+    results directory - a full path to results folder
+    gt directory - a full path to ground truth folder
+    list path - a full path to images list (without extensions)
+    """
 
+    classes = []
+    results_directory = None
+    gt_directory = None
+    list_path = None
 
-	def calculate_image_IoU(self, image, gt, class_index):
-		assert(image.mode in ['1', 'L', 'P'])
-		assert(gt.mode in ['1', 'L', 'P'])
-		
-		image_mask = image.point(lambda p: (p == class_index) * 255)
-		gt_mask = gt.point(lambda p: (p == class_index) * 255)
-		gt_mask = gt_mask.convert('1')
+    def check_segmentation(self, image):
+        """
+        Returns an image intersection and union scores
+        """
 
-		intersection = Image.new('1', image.size, 0)
-		intersection.paste(image_mask, gt_mask)
+        assert(image.mode in ['1', 'L', 'P'])
+        image_mask = image.point(
+            lambda p: (len(self.classes) <= p) and (p < 255))
 
-		stat = ImageStat.Stat(intersection)
-		intersection_count = stat.sum[0] / 255
+        stat = ImageStat.Stat(image_mask)
+        if (0 < stat.sum[0]):
+            raise Exception('Image has an unexpected class index')
 
-		stat = ImageStat.Stat(image_mask)
-		image_count = stat.sum[0] / 255
+    def process(self):
+        """
+        Processes the given set of images and returns an IoU score.
+        Throws an exception on wrong input parameters
+        """
 
-		stat = ImageStat.Stat(gt_mask)
-		gt_count = stat.sum[0] / 255
+        if (self.results_directory is None):
+            raise Exception('Segmentation directory is not set')
+        if (self.gt_directory is None):
+            raise Exception('Ground Truth directory is not set')
+        if (self.list_path is None):
+            raise Exception('Images list path is not set')
 
-		union_count = image_count + gt_count - intersection_count
-		IoU = 0
-		if (union_count != 0):
-			IoU = intersection_count / union_count
+        images_list = open(self.list_path, 'r')
+        confcounts = numpy.zeros((len(self.classes), len(self.classes)))
 
-		presence = (gt_count != 0)
+        for entry in images_list:
+            entry = entry.strip()
+            print('Processing entry \'%s\'' % (entry))
 
-		return (IoU, presence)
+            result_filepath = os.path.join(
+                self.results_directory, entry + os.extsep + 'png')
+            gt_filepath = os.path.join(
+                self.gt_directory, entry + os.extsep + 'png')
 
+            # load groudtruth image
+            gtim = numpy.asarray(
+                Image.open(gt_filepath).convert('L')).astype(float)
+            # load segmentation image
+            resim = numpy.asarray(
+                Image.open(result_filepath).convert('L')).astype(float)
 
-	def check_segmentation(self, image):
-		assert(image.mode in ['1', 'L', 'P'])
-		image_mask = image.point(lambda p: (len(self.Classes) <= p) and (p < 255))
-		stat = ImageStat.Stat(image_mask)
+            if ((resim.shape[0] != gtim.shape[0]) or
+                    (resim.shape[1] != gtim.shape[1])):
+                raise Exception("Image sizes are different")
 
-		if (0 < stat.sum[0]):
-			raise Exception('Image has an unexpected class index')
+            sumim = 1 + gtim + resim * len(self.classes)
+            locs = gtim < 255
+            maskedsumim = numpy.extract(locs, sumim)
+            [hs, bin_edges] = numpy.histogram(
+                maskedsumim,
+                bins=range(1, len(self.classes) * len(self.classes) + 2))
+            confcounts += numpy.reshape(hs,
+                                        (len(self.classes), len(self.classes)))
 
+        self.overall_accuracy = 100 * \
+            numpy.sum(numpy.diagonal(confcounts)) / numpy.sum(confcounts)
 
-	def process_image(self, image_path, gt_path):
-		IoUs = [(0.0, 0)] * len(self.Classes) # (IoU, class presence)
-		
-		image = Image.open(image_path)
-		image = image.convert('L') # grayscale
-		self.check_segmentation(image)
+        # class accuracy
+        self.class_acc = numpy.zeros(len(self.classes))
+        denoms = numpy.sum(confcounts, axis=0)
+        for i in range(0, len(self.classes)):
+            if (denoms[i] == 0):
+                denoms[i] = 1
+            self.class_acc[i] = 100 * confcounts[i][i] / denoms[i]
+        self.aver_class_acc = numpy.sum(self.class_acc) / len(self.classes)
 
-		gt = Image.open(gt_path)
-		gt = gt.convert('P') # palette-based
-		gt = gt.point(lambda p: (p < 255) * p)
-		self.check_segmentation(gt)
+        # pixel IoU
+        self.accuracies = numpy.zeros(len(self.classes))
+        gtj = numpy.sum(confcounts, axis=1)
+        resj = numpy.sum(confcounts, axis=0)
+        for j in range(0, len(self.classes)):
+            gtjresj = confcounts[j][j]
+            denom = gtj[j] + resj[j] - gtjresj
+            if (denom == 0):
+                denom = 1
+            self.accuracies[j] = 100 * gtjresj / denom
+        self.aver_accuracy = numpy.sum(self.accuracies) / len(self.classes)
 
-		if (image.width != gt.width or image.height != gt.height):
-			raise Exception("Image sizes are different")
-
-		for class_index in range(len(self.Classes)):
-			IoUs[class_index] = self.calculate_image_IoU(image, gt, class_index)
-
-		return IoUs
-
-
-	def process(self):
-		if (self.results_directory == None):
-			raise Exception('Segmentation directory is not set')
-		if (self.gt_directory == None):
-			raise Exception('Ground Truth directory is not set')
-		if (self.list_path == None):
-			raise Exception('Images list path is not set')
-
-		files_count = 0
-		classes_metrics = [0.0] * len(self.Classes)
-		classes_presented = [0] * len(self.Classes)
-		
-		images_list = open(self.list_path, 'r')
-
-		for entry in images_list:
-			entry = entry.strip()
-			print('Processing entry \'%s\'' % (entry))
-
-			result_filepath = os.path.join(self.results_directory, entry + os.extsep + 'png')
-			gt_filepath = os.path.join(self.gt_directory, entry + os.extsep + 'png')
-
-			current_metrics = self.process_image(result_filepath, gt_filepath)
-			for i in range(len(self.Classes)):
-				classes_metrics[i] += current_metrics[i][0]
-				classes_presented[i] += current_metrics[i][1]
-
-			files_count += 1
-
-
-		overall_accuracy = 0.0
-		overall_classes_presented = 0
-		for i in range(len(self.Classes)):
-			if (0 < classes_presented[i]):
-				classes_metrics[i] /= classes_presented[i]
-
-		for i in range(1, len(self.Classes)):				
-			overall_classes_presented += (classes_presented[i] != 0)
-			overall_accuracy += classes_metrics[i]
-
-		if (0 < overall_classes_presented):
-			overall_accuracy /= overall_classes_presented
-
-		self.classes_metrics = classes_metrics
-		self.overall_accuracy = overall_accuracy
-
-
-	def show_results(self):
-		print('Segmentation accuracy (IoU metric)')
-		print('Overall accuracy: %6.3f%%' % (100.0 * self.overall_accuracy))
-		print('Per class accuracy:')
-		for i in range(len(self.Classes)):
-			print('  %14s: %6.3f%%' % \
-				(self.Classes[i] , 100.0 * self.classes_metrics[i]) )
+    def show_results(self):
+        print('----------------------------------')
+        print('Percentage of pixels correctly labelled overall: %6.3f%%' %
+              self.overall_accuracy)
+        print('----------------------------------')
+        print('Mean Class Accuracy:: %6.3f%%' % self.aver_class_acc)
+        print('Per class accuracy:')
+        for i in range(len(self.classes)):
+            print('  %14s: %6.3f%%' %
+                  (self.classes[i], self.class_acc[i]))
+        print('----------------------------------')
+        print('Average accuracy: %6.3f%%' % self.aver_accuracy)
+        print('Accuracy for each class (intersection/union measure)')
+        for i in range(len(self.classes)):
+            print('  %14s: %6.3f%%' %
+                  (self.classes[i], self.accuracies[i]))
+        print('----------------------------------')
 
 
 if (__name__ == '__main__'):
-	if (len(sys.argv) <= 1):
-		show_help()
-		exit()
+    if (len(sys.argv) != 4):
+        show_help()
+        exit()
 
-	processor = SegmentationResultsProcessor()
-	processor.results_directory = os.path.abspath(sys.argv[1])
-	processor.gt_directory = os.path.abspath(sys.argv[2])
-	processor.list_path = os.path.abspath(sys.argv[3])
-	processor.process()
-	processor.show_results()
+    processor = SegmentationResultsProcessor()
+    processor.results_directory = os.path.abspath(sys.argv[1])
+    processor.gt_directory = os.path.abspath(sys.argv[2])
+    processor.list_path = os.path.abspath(sys.argv[3])
+    processor.classes = [
+        'background',  # keep first
+        'aeroplane',
+        'bicycle',
+        'bird',
+        'boat',
+        'bottle',
+        'bus',
+        'car',
+        'cat',
+        'chair',
+        'cow',
+        'diningtable',
+        'dog',
+        'horse',
+        'motorbike',
+        'person',
+        'pottedplant',
+        'sheep',
+        'sofa',
+        'train',
+        'tvmonitor'
+    ]
+    processor.process()
+    processor.show_results()
